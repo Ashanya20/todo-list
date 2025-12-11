68fd81f8276e2e35cc66df9b0269757bf95d7473
Ashanya20 <92543385+Ashanya20@users.noreply.github.com>
2025-12-10 18:24:34 -0800

    Week 5

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,8 +1,18 @@
-function TodoForm() {
+import { useRef } from 'react';
+
+function TodoForm({ onAddTodo }) {
+  const todoTitleInput = useRef('');
+  function handleAddTodo(event) {
+    event.preventDefault(); // prevent the page from refreshing
+    const title = event.target.title.value;
+    onAddTodo(title);
+    event.target.title.value = ''; // clear input
+    todoTitleInput.current.focus(); // refocus input
+  }
   return (
-    <form>
+    <form onSubmit={handleAddTodo}>
       <label htmlFor="todoTitle">Todo</label>
-      <input type="text" id="todoTitle" />
+      <input ref={todoTitleInput} type="text" id="todoTitle" name="title" />
       <button type="submit">Add Todo</button>
     </form>
   );
